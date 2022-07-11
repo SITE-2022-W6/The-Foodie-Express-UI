295d9629b388a1fb2830442a85c89759e514d6ce
acordero4852 <corderoandy5@gmail.com>
2022-07-11 14:21:01 -0400

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import './Navbar';
+
+export default function Navbar(props) {
+  return (
+    <div className="navbar">
+      <p>{'<Navbar/>'}</p>
+    </div>
+  );
+}
---
+++ src/services/apiClient.jsx
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+class ApiClient {
+  constructor() {}
+
+  setToken(token) {}
+
+  async request({ endpoint, method = 'GET', data = {} }) {}
+}
+
+export default new ApiClient('');
