fc17a89f5fb8687067161c1f614317138610e15e
WinsonChen114 <14winsonchen@gmail.com>
2022-08-04 17:59:27 -0400

    fixed getting user name for a review

--- src/components/ItemView/ItemView.jsx
+++ src/components/ItemView/ItemView.jsx
@@ -8,35 +8,38 @@ import { useParams, Navigate } from 'react-router-dom';
 import './ItemView.css';
 
 export default function ItemView(props) {
-  {/* The useState for toggling modal popup */}
+  {/* The useState for toggling modal popup */ }
   const [isOpen, setIsOpen] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [restaurantInfo, setRestaurntInfo] = useState({});
   const [item, setItem] = useState({});
   const [ratingValue, setRatingValue] = useState(0)
   const [comment, setComment] = useState('')
   const [reviews, setReviews] = useState([])
   const [error, setError] = useState('')
-  const {restaurantId, itemName} = useParams()
+  const { restaurantId, itemName } = useParams()
+  const [userName, setUserName] = React.useState()
 
   useEffect(() => {
     async function getItem(r, i) {
       setIsLoading(true);
       const itemInfo = await apiClient.getMenuItem(r, i);
       setItem(itemInfo.data.item);
-      setIsLoading(false);
       getReviews(r, i)
     };
 
     async function getReviews(r, i) {
       window.scrollTo(0, 0)
-      setIsLoading(true);
       const listOfReviews = await apiClient.getReviewsForItem(r, i);
       setReviews(listOfReviews.data.reviews);
       setIsLoading(false);
     };
     getItem(restaurantId, itemName)
-  }, [restaurantId, itemName]);
+    apiClient.getMenuByOpenMenuId(restaurantId)
+  .then(restaurant => {
+   setRestaurntInfo(restaurant.data.menu.restaurant_info)
+  })
+  }, []);
 
   const handleChange = (e) => {
     setComment(e.target.value)
@@ -45,10 +48,8 @@ export default function ItemView(props) {
   const handleRatingChange = (value) => {
     setRatingValue(value)
   }
-  //apiClient.getMenuByOpenMenuId(restaurantId)
-  //.then(restaurant => {
-  //  setRestaurntInfo(restaurant.data.menu.restaurant_info)
-  //})
+  
+
 
   const handleSubmit = async () => {
     setIsLoading(true)
@@ -61,9 +62,9 @@ export default function ItemView(props) {
     });
     if (error) {
       return
-    } 
+    }
     if (data?.review) {
-      
+
     }
     setRatingValue(0);
     setComment('');
@@ -79,22 +80,22 @@ export default function ItemView(props) {
       sum += review.rating;
       ++len;
     });
-    return (sum/len).toFixed(1);
+    return (sum / len).toFixed(1);
   }
 
   return (
-    <div className="center navbar-margin-top">
+    <> {!isLoading && <div className="center navbar-margin-top">
       <div className="item-view">
         <div className="item-heading">
-        {/* The heading of item */}
+          {/* The heading of item */}
           <h1 style={{}}>{item.name}</h1>
           <h3>{restaurantInfo.restaurant_name} at {restaurantInfo.address_1}, {restaurantInfo.city_town}, {restaurantInfo.state_province}</h3>
           <Rating
             initialRating={avgRatingValue()}
             fractions={10}
-            placeholderSymbol={<BsStarFill size={20}/>}
-            emptySymbol={<BsStar size={20}/>}
-            fullSymbol={<BsStarFill size={20}/>}
+            placeholderSymbol={<BsStarFill size={20} />}
+            emptySymbol={<BsStar size={20} />}
+            fullSymbol={<BsStarFill size={20} />}
             readonly
           />
         </div>
@@ -103,16 +104,16 @@ export default function ItemView(props) {
           {/* A button to add a review via modal popup */}
           <button className="btn" onClick={() => setIsOpen(true)}>Add Review</button>
           <Modal open={isOpen} onClose={() => setIsOpen(false)}>
-            {!props.isAuthenticated && <Navigate to="/login" /> }
-            <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'column', flexWrapper: 'wrap'}}>
+            {!props.isAuthenticated && <Navigate to="/login" />}
+            <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', flexWrapper: 'wrap' }}>
               <h1>Write a Review</h1>
               <Rating
                 initialRating={ratingValue}
-                emptySymbol={<BsStar size={20}/>}
-                fullSymbol={<BsStarFill size={20}/>}
+                emptySymbol={<BsStar size={20} />}
+                fullSymbol={<BsStarFill size={20} />}
                 onChange={handleRatingChange}
               />
-              <textarea cols="40" rows="6" style={{resize: 'none'}} onChange={handleChange}></textarea>
+              <textarea cols="40" rows="6" style={{ resize: 'none' }} onChange={handleChange}></textarea>
               <button className="btn .login button" onClick={handleSubmit}>Submit your Review</button>
             </div>
           </Modal>
@@ -120,40 +121,41 @@ export default function ItemView(props) {
         <h2>Review</h2>
         <div className="review-session">
           {/* A list of reviews on the item */}
-          {reviews && reviews.map(review => {
-            return (<Review comment={review.content} date={review.created_at} rating={review.rating} user_id={review.user_id}/>)
-          })}
+          {reviews && <Reviews reviews={reviews}/>}
         </div>
       </div>
-    </div>
+    </div>}
+    </>
   )
 }
 
-export function Review(props) {
-  const [userName, setUserName] = React.useState()
-  //apiClient.getUserByUserId(props.user_id)
-  //.then(result => {
-  //  setUserName(`${result.first_name} ${result.last_name}`)
-  //})
-  let date = props.date.split("-")
-  date[2] = date[2].substring(0,2)
-  return (
-    <div className="item-review">
-      <div className="header">
-        <div className="name-date">
-          <h3>{userName}</h3>
-          <p>{`${Number(date[1])} / ${Number(date[2])} / ${Number(date[0])}`}</p>
-        </div>
-        <Rating
-          initialRating={props.rating}
-          fractions={2}
-          placeholderSymbol={<BsStarFill size={20}/>}
-          emptySymbol={<BsStar size={20}/>}
-          fullSymbol={<BsStarFill size={20}/>}
-          readonly
-        />
-      </div>
-      <div className="review-content">{props.comment}</div>
-    </div>
-  )
+export function Reviews({ reviews }) {
+  return reviews.map((review, idx) => {
+    const userName = review.first_name + " "+ review.last_name
+    return (<Review key={idx} comment={review.content} date={review.created_at} rating={review.rating} userName={userName} />)
+  })
 }
+
+export function Review(props) {
+    let date = props.date.split("-")
+    date[2] = date[2].substring(0, 2)
+    return (
+      <div className="item-review">
+        <div className="header">
+          <div className="name-date">
+            <h3>{props.userName}</h3>
+            <p>{`${Number(date[1])} / ${Number(date[2])} / ${Number(date[0])}`}</p>
+          </div>
+          <Rating
+            initialRating={props.rating}
+            fractions={2}
+            placeholderSymbol={<BsStarFill size={20} />}
+            emptySymbol={<BsStar size={20} />}
+            fullSymbol={<BsStarFill size={20} />}
+            readonly
+          />
+        </div>
+        <div className="review-content">{props.comment}</div>
+      </div>
+    )
+  }
